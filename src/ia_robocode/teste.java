bd40ede465203ce206aa13482c15e28ea5f04c3e
gabrielvba <gabriel.alves@ccc.ufcg.edu.br>
2018-10-23 10:54:41 -0300

    projeto robocode no eclipse

---
+++ src/ia_robocode/teste.java
@@ -0,0 +1,14 @@
+package ia_robocode;
+
+import robocode.AdvancedRobot;
+
+
+public class teste extends AdvancedRobot {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
+	
+	
+}
